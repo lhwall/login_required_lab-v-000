4accd3a5e89358eccf02acd800c852a1439e63a2
Lucy Wall <lhwall@gmail.com>
2018-07-23 23:32:48 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ def create
   if params[:name] == nil || params[:name] == ""
     redirect_to controller: 'sessions', action: 'new'
 else
+  session[:name] = params[:name]
   redirect_to controller: "sessions", action: "welcome"
 end
 end
